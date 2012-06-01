be579f265bf86ace6b54d54cce9ddddf9fb7865d
josh <pgmjsd@gmail.com>
2012-06-01 15:17:19 -0400

    switch to JUnit because IDEA doesn't like the new version of TestNG try to get Arquillian Persistence working with embedded glassfish.... still no luck!

--- arq-persistence/src/test/java/org/yajul/eg/arq/JUnitArquillianPersistenceTest.java
+++ arq-persistence/src/test/java/org/yajul/eg/arq/JUnitArquillianPersistenceTest.java
@@ -1,43 +1,55 @@
-package org.yajul.eg.jap.arqtestng;
+package org.yajul.eg.arq;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.yajul.eg.jpa.basic.Thing;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 /**
- * Arquillian Persistence example with TestNG
+ * Arquillian Persistence example with JUnit
  * <br>
  * User: josh
  * Date: 4/24/12
  * Time: 7:39 PM
  */
-@Test
-public class TestNGArquillianPersistenceExample extends Arquillian
+
+@RunWith(Arquillian.class)
+public class JUnitArquillianPersistenceTest
 {
     @Deployment
     public static Archive<?> createDeploymentPackage()
     {
         return ShrinkWrap.create(JavaArchive.class, "test.jar")
                 .addPackage(Thing.class.getPackage())
-                .addAsManifestResource("test-persistence.xml", "persistence.xml");
+                .addAsManifestResource("test-persistence.xml", "persistence.xml")
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
 
     @PersistenceContext
     private EntityManager em;
 
+    @Inject
+    private UserTransaction utx;
+
+    @Test
     public void testThing() throws Exception
     {
+        utx.begin();
         Thing thing = new Thing();
         em.persist(thing);
+        utx.commit();
 
-        System.out.println("tx.isActive()=" + em.getTransaction().isActive() );
+        System.out.println("tx.isActive()=" + em.getTransaction().isActive());
     }
 }
--- simple-embedded-container/src/test/java/org/yajul/eg/arq/SimpleArqTest.java
+++ simple-embedded-container/src/test/java/org/yajul/eg/arq/SimpleArqTest.java
@@ -1,11 +1,12 @@
 package org.yajul.eg.arq;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
 import javax.transaction.UserTransaction;
@@ -17,8 +18,8 @@ import javax.transaction.UserTransaction;
  * Date: 5/2/12
  * Time: 3:32 PM
  */
-@Test
-public class SimpleArqTest extends Arquillian
+@RunWith(Arquillian.class)
+public class SimpleArqTest
 {
 
     @Inject
@@ -35,6 +36,7 @@ public class SimpleArqTest extends Arquillian
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
+    @Test
     public void testSimpleBean()
     {
         String s = simple.hello();
