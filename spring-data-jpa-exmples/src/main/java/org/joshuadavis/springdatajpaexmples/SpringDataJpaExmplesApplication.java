6414e68591b7e16c9d705d9d459599979298c86a
Joshua Davis <joshua.davis@parityenergy.com>
2020-10-16 11:30:15 -0400

    add spring boot jpa project skeleton

---
+++ spring-data-jpa-exmples/src/main/java/org/joshuadavis/springdatajpaexmples/SpringDataJpaExmplesApplication.java
@@ -0,0 +1,13 @@
+package org.joshuadavis.springdatajpaexmples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaExmplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaExmplesApplication.class, args);
+	}
+
+}
---
+++ spring-data-jpa-exmples/src/test/java/org/joshuadavis/springdatajpaexmples/SpringDataJpaExmplesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.joshuadavis.springdatajpaexmples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaExmplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
